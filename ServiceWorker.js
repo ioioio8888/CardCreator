9f773739187e3463f3128ec08aa0bc2a935bee61
ioioio8888 <louislui95111@gmail.com>
2024-12-12 19:46:44 +0800

    [Update] webgl try fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "ioioio8888-CardMaker-0.1.0";
 const contentToCache = [
-    "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/CardCreator.loader.js",
+    "Build/CardCreator.framework.js",
+    "Build/CardCreator.data",
+    "Build/CardCreator.wasm",
     "TemplateData/style.css"
 
 ];
